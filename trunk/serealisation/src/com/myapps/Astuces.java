a6a4af59e845b5b32560611db2baee9bd1824908
romain.solve <romain.solve@9efb9dfa-d3c1-5de3-1e8a-5721dd0c4fa1>
2011-03-02 22:13:16 +0000

    astuces aleatoire et defilement + design

---
+++ trunk/serealisation/src/com/myapps/Astuces.java
@@ -0,0 +1,23 @@
+package com.myapps;
+public class Astuces {
+	
+	private String s[] = {
+			"Appuyez Sur Menu pour accéder aux options",
+			"Vous pouvez visualiser jusqu'à 4 caméras",
+			"En touchant l'écran vous pouvez echanger les cameras", 
+			"De prochaines astuces bientôt disponibles",
+			"Sauvegarder vos caméras dans un fichier. Ca évitera de les rentrer à chaque fois"} ;
+		
+	private int max = 5 ;
+	Astuces(){
+	}
+	
+	public String getLabel(int i) {
+	      return s[i] ;
+	   }
+	
+	public int getMax(){
+		return max;
+	}
+	
+}
--- trunk/serealisation/src/com/myapps/Dialog_welcome.java
+++ trunk/serealisation/src/com/myapps/Dialog_welcome.java
@@ -1,11 +1,13 @@
 package com.myapps;
-import android.R.id;
+import java.util.Random;
+
 import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Message;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 public class Dialog_welcome extends Dialog{
@@ -13,11 +15,10 @@ public class Dialog_welcome extends Dialog{
 	public interface ReadyListener {
         public void ready(String name);
     }
-
+	
     private String name;
-    private ReadyListener readyListener;
-    EditText etName;
-
+    private String cheats ;
+    private TextView text ;
     public Dialog_welcome(Context context, String name) {
         super(context);
         this.name = name;
@@ -28,17 +29,40 @@ public class Dialog_welcome extends Dialog{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dialog_welcome);
         setTitle(name);
-        TextView text = (TextView) findViewById(R.id.text);
-        text.setText("Astuce : Appuyez sur la touche Menu pour accéder aux options");
+        text = (TextView) findViewById(R.id.text);
+        
+        Random randomGenerator = new Random() ;
+        Astuces a = new Astuces() ;
+        int randomInt = randomGenerator.nextInt(100)%a.getMax();
+
+        text.setText("Astuce : ") ;
+        cheats = a.getLabel(randomInt) ;
+        text.append(cheats) ;
+
+        
         Button buttonOK = (Button) findViewById(R.id.ok);
         buttonOK.setOnClickListener(new OKListener());
+        
+        Button right = (Button) findViewById(R.id.arrow_right) ;
+        right.setOnClickListener(new RightListener());
     }
 
     private class OKListener implements android.view.View.OnClickListener {
         @Override
         public void onClick(View v) {
-            //readyListener.ready(String.valueOf(etName.getText()));
             Dialog_welcome.this.dismiss();
+        }
+    }
+    
+    private class RightListener implements android.view.View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            Random randomGenerator = new Random() ;
+            Astuces a = new Astuces() ;
+            int randomInt = randomGenerator.nextInt(100)%a.getMax();
+            text.setText("Astuce : ") ;
+            cheats = a.getLabel(randomInt) ;
+            text.append(cheats) ;
         }
     }
 
