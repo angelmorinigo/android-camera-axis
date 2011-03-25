76ac2daa1475b3e690413f0416ca690219aafb7f
nahelou.jerome <nahelou.jerome@gmail.com>
2011-03-25 07:49:55 +0000

    fix force clase

--- source-camera/src/com/myapps/Astuces.java
+++ source-camera/src/com/myapps/Astuces.java
@@ -3,33 +3,24 @@ package com.myapps;
 import android.content.Context;
 
 public class Astuces {
-	
-	private String[] s;	
-	private int max = 5 ;
+
+	private String[] s;
+	private int max = 12;
 	private Context context;
-	
+
 	public Astuces(Context mContext) {
 		context = mContext;
-		s[0] = context.getString(R.string.astuce1);
-		s[1] = context.getString(R.string.astuce2);
-		s[2] = context.getString(R.string.astuce3);
-		s[3] = context.getString(R.string.astuce4);
-		s[4] = context.getString(R.string.astuce5);
-		s[5] = context.getString(R.string.astuce6);
-		s[6] = context.getString(R.string.astuce7);
-		s[7] = context.getString(R.string.astuce8);
-		s[8] = context.getString(R.string.astuce9);
-		s[9] = context.getString(R.string.astuce10);
-		s[10] = context.getString(R.string.astuce11);
-		s[11] = context.getString(R.string.astuce12);
+		s = new String[max];
+		for (int i = 0; i < max; i++)
+			s[i] = context.getString(R.string.astuce1 + i);
 	}
-	
+
 	public String getLabel(int i) {
 		return s[i];
 	}
-	
+
 	public int getMax() {
 		return max;
 	}
-	
+
 }
